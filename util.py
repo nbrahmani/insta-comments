1d34fbc182b12aab34efc7d369538428454c8deb
Brahmani Nutakki <55019129+nbrahmani@users.noreply.github.com>
2025-12-29 14:52:07 +0100

    book keeping

--- util.py
+++ util.py
@@ -1,5 +1,6 @@
 import os
 import re
+import ast
 import json
 import pandas as pd
 import numpy as np
@@ -34,13 +35,12 @@ def clean_comments(comments):
         match = re.search(pattern, x)
         if match:
             comments.remove(x)
-            break
+
     pattern = r'[\d,]+\s+comments? from Facebook'
     for x in comments:
         match = re.search(pattern, x)
         if match:
             comments.remove(x)
-            break
     
     # get number of likes. usually at last but sometimes it shifts.
     pattern = r'\b\d[\d,]*\s+likes?\b'
@@ -49,7 +49,6 @@ def clean_comments(comments):
         if match:
             likes = x
             comments.remove(x)
-            break
 
     # remove comments about cookies. This happens when there are less no of of comments.
     cookie_texts = ['We use cookies and similar technologies to help provide and improve content on Meta Products. We also use them to provide a safer experience by using information that we receive from cookies on and off Instagram, and to provide and improve Meta Products for people who have an account.',
@@ -66,12 +65,38 @@ def clean_comments(comments):
                     '•',
                     'You have control over the optional cookies that we use. Learn more about cookies and how we use them in our Cookies Policy.',
                     'We use cookies on apps and websites owned by other companies that use Meta technologies. These cookies help other companies share information with us about your activity on their apps and websites.',
+                    'See photos, videos and more from NBC Entertainment.',
+                    'See photos, videos and more from Peacock.',
+                    'See photos, videos and more from Bel-Air on Peacock.',
+                    'See photos, videos and more from Spotify.',
+                    'See photos, videos and more from NYT Cooking.',
+                    'See photos, videos and more from ESPN.',
+                    'See photos, videos and more from Cat Lovers Club.',
+                    'See photos, videos and more from The Dogist.',
+                    'See photos, videos and more from Kayo Sports.',
+                    'See photos, videos and more from Access Hollywood.',
+                    'See photos, videos and more from LADbible.',
+                    'See photos, videos and more from Percy Jackson.',
+                    'See photos, videos and more from Spectrum.',
+                    'See photos, videos and more from Kumail Nanjiani.',
+                    'See photos, videos and more from Hulu.',
+                    'See photos, videos and more from ESPN College Football.',
+                    'See photos, videos and more from Empire State Building.',
+                    'See photos, videos and more from The Grade Cricketer.',
+                    'See photos, videos and more from Triple M Cricket.',
+                    'See photos, videos and more from PBS Food.',
+                    'See photos, videos and more from Freeform.',
+                    'See photos, videos and more from SPORTbible.',
+                    'See photos, videos and more from PBS SoCal.',
+                    'See photos, videos and more from Cat Lovers Club.',
                     ]
+    
     remove_list = []
     for x in comments:
         for ct in cookie_texts: 
             if x.strip() == ct.strip():
                 remove_list.append(x)
+    remove_list = list(set(remove_list))
 
     for x in remove_list:
         comments.remove(x)
@@ -123,11 +148,13 @@ def compute_fprops(crawl1, crawl2, merged_df, all_urls):
                     s1_no_rank = s1
                     s1_rank = s1
 
+                # diff_s_no_rank = set(s1_no_rank).intersection(set(s2))
                 diff_s_no_rank = set(s1_no_rank).symmetric_difference(set(s2))
                 diff_s_rank = [1 for x, y in zip(s1_rank, s2) if x != y]
 
-                denom_no_rank = len(set(s1_no_rank).union(set(s2)))
-                # denom_no_rank = max(len(s1_rank), len(s2))
+                denom_no_rank = len(s1_no_rank) + len(s2) 
+                # denom_no_rank = len(set(s1_no_rank).union(set(s2)))
+                # denom_no_rank = min(len(s1_rank), len(s2))
                 denom_rank = max(len(s1_rank), len(s2))
 
                 fprop_no_rank = len(diff_s_no_rank) / denom_no_rank
@@ -143,7 +170,7 @@ def compute_fprops(crawl1, crawl2, merged_df, all_urls):
                 merged_df.loc[(merged_df['account'] == account) & (merged_df['urlid'] == urlid), 'ftrials_rank'] = denom_rank
 
             except Exception as e:
-                print(e, account, urlid)
+                print(e, account, urlid, denom_no_rank, denom_rank)
                 continue
         account_fprops_no_rank[account] = temp_list_no_rank
         account_fprops_rank[account] = temp_list_rank
@@ -206,53 +233,202 @@ def get_chronological_order_comments(merged_df, all_urls, all_crawls):
     return temp_df
 
 
-def compute_label_comments(crawl1, crawl2, merged_df, all_urls):
-    if crawl2 == 'chronological':
-        raise ValueError('chronological should be the first crawl')
+def compute_label_reg(crawl1, crawl2):
+    gender_labels = ['female_female', 'female_male', 'male_male', 'none_female', 'none_male', 'none_none']
+    ideology_labels = ['dem_dem', 'dem_rep', 'rep_rep', 'none_dem', 'none_rep', 'none_none']
+    location_labels = ['ny_ny', 'ny_texas', 'texas_texas', 'none_ny', 'none_texas', 'none_none']
+    gender, ideology, location = '', '', ''
 
-    col1 = f'{crawl1}_comment'
-    col2 = f'{crawl2}_comment'
+    try:
+        gender1 = re.search(r"(female|male)", crawl1, re.I).group(1).lower() 
+    except:
+        gender1 = 'none'
+    try:
+        gender2 = re.search(r"(female|male)", crawl2, re.I).group(1).lower()  
+    except:
+        gender2 = 'none'
 
-    for account in all_urls.keys():
-        urlids = merged_df[merged_df['account'] == account]['urlid'].unique()
+    gender_str = gender1 + '_' + gender2
+    gender_str_rev = gender2 + '_' + gender1
 
-        temp_list_no_rank = []
-        temp_list_rank = []
-        for urlid in urlids:
-            # dropping NaN comments which were here due to the addition of the chronological comments
-            s1 = merged_df[(merged_df['account'] == account) & (merged_df['urlid'] == urlid)][col1].dropna().tolist()
-            s2 = merged_df[(merged_df['account'] == account) & (merged_df['urlid'] == urlid)][col2].dropna().tolist()
+    if gender_str in gender_labels:
+        gender = gender_str
+    elif gender_str_rev in gender_labels:
+        gender = gender_str_rev
 
-            # need to implement diff structure for chronological comments since they will have comments from all crawls 
-            try:
-                if crawl1 == 'chronological':
-                    s1_no_rank = s1[:len(s2)]
-                    s1_rank = [x for x in s1 if x in s2]
-                else:
-                    s1_no_rank = s1.copy()
-                    s1_rank = s1.copy()
-                
-                for x in s2:
-                    if x in s1_no_rank:
-                        label_no_rank = 1
-                    else :
-                        label_no_rank = 0
-                    
-                    cidx = merged_df.index[merged_df[col2] == x].values[0]
-                    cnum = merged_df.loc[cidx, 'comment_num']
-                    merged_df.loc[(merged_df['account'] == account) & (merged_df['urlid'] == urlid) & (merged_df['comment_num'] == cnum), 'fcount_no_rank'] = label_no_rank
-                
-                for i, x in enumerate(s2):
-                    if x == s1_rank[i]:
-                        label_rank = 1 
-                    else:
-                        label_rank = 0
-                        
-                    cidx = merged_df.index[merged_df[col2] == x].values[0]
-                    cnum = merged_df.loc[cidx, 'comment_num']
-                    merged_df.loc[(merged_df['account'] == account) & (merged_df['urlid'] == urlid) & (merged_df['comment_num'] == cnum), 'fcount_rank'] = label_rank
+    try:
+        ideology1 = re.search(r"(dem|rep)", crawl1, re.I).group(1).lower() 
+    except:
+        ideology1 = 'none'
+    try:
+        ideology2 = re.search(r"(dem|rep)", crawl2, re.I).group(1).lower()  
+    except:
+        ideology2 = 'none'
 
-            except Exception as e:
-                print(e, account, urlid)
+    ideology_str = ideology1 + '_' + ideology2
+    ideology_str_rev = ideology2 + '_' + ideology1
+
+    if ideology_str in ideology_labels:
+        ideology = ideology_str
+    elif ideology_str_rev in ideology_labels:
+        ideology = ideology_str_rev
+
+    try:
+        location1 = re.search(r"(ny|texas)", crawl1, re.I).group(1).lower() 
+    except:
+        location1 = 'none'
+    try:
+        location2 = re.search(r"(ny|texas)", crawl2, re.I).group(1).lower()  
+    except:
+        location2 = 'none'
+
+    location_str = location1 + '_' + location2
+    location_str_rev = location2 + '_' + location1
+
+    if location_str in location_labels:
+        location = location_str
+    elif location_str_rev in location_labels:
+        location = location_str_rev
+
+    return gender, ideology, location
+
+
+def get_df_for_regression(df, all_urls, all_crawls):
+
+    reg_df = pd.DataFrame(columns=['account', 'urlid', 'gender', 'ideology', 'fcount_no_rank', 'ftrials_no_rank', 'fcount_rank', 'ftrials_rank'])
+
+    temp_list = []
+    for crawl1 in all_crawls:
+        for crawl2 in all_crawls:
+
+            if crawl1 == crawl2:
                 continue
-    return merged_df
+
+            temp_dict = {}
+
+            _, _, mod_df = compute_fprops(crawl1, crawl2, df, all_urls)
+            temp_df = mod_df[['account', 'urlid', 'fcount_no_rank', 'ftrials_no_rank', 'fcount_rank', 'ftrials_rank']].drop_duplicates()
+
+            gender, ideology, location = compute_label_reg(crawl1, crawl2)
+
+            temp_dict['location'] = location
+            temp_dict['gender'] = gender
+            temp_dict['ideology'] = ideology
+            
+            # var1 = min(crawl1, crawl2)
+            # var2 = max(crawl1, crawl2)
+            # temp_dict['setup_a'] = var1
+            # temp_dict['setup_b'] = var2
+            # temp_dict['pair_id'] = f'{var1}_{var2}'
+
+            for index, row in temp_df.iterrows():
+                temp_dict['urlid'] = row['urlid']
+                temp_dict['account'] = row['account']
+                temp_dict['fcount_no_rank'] = row['fcount_no_rank']
+                temp_dict['ftrials_no_rank'] = row['ftrials_no_rank']
+                temp_dict['fcount_rank'] = row['fcount_rank'] 
+                temp_dict['ftrials_rank'] = row['ftrials_rank']
+                temp_list.append(temp_dict.copy())
+
+
+    reg_df = pd.DataFrame(temp_list)       
+    return reg_df
+
+
+def numify_metrics(val):
+    if pd.notna(val):
+        val = val.lower()
+        if 'k' in val:
+            val = val.replace('k', '')
+            num = float(val)*1000
+        else:
+            num = float(val)
+        return num
+    else:
+        return val
+
+
+def compute_presence_comments(crawl1, crawl2, df, labels_df):
+    col1 = f'{crawl1}_comment'
+    col2 = f'{crawl2}_comment' 
+
+    mod_df = pd.DataFrame(columns=['account', 'urlid', 'comment', 'presence_no_rank', 'presence_rank', 'label'])
+
+    temp_list = []
+    for account in df['account'].unique():
+        urlids = df[df['account'] == account]['urlid'].unique()
+
+        for urlid in urlids:
+            # TODO: if using chronological, need to think here
+            temp = df[(df['account'] == account) & (df['urlid'] == urlid)].copy()
+            s1 = temp[col1].dropna().tolist()
+            s2 = temp[col2].dropna().tolist()
+
+            unique_comments = set(s1).intersection(set(s2))
+
+            # TODO: need to include code for rankign if needed
+            for comment in set(s1).union(set(s2)):
+                temp_dict = {}
+                if pd.isna(comment):
+                    continue
+                else:
+                    if comment in unique_comments:
+                        presence_no_rank = 'Yes'
+                    else:
+                        presence_no_rank = 'No'
+
+                    label = labels_df[labels_df['comment_combined'] == ast.literal_eval(comment)]['label'].values[0]
+                    
+                    temp_dict['account'] = account
+                    temp_dict['urlid'] = urlid
+                    temp_dict['comment'] = comment
+                    temp_dict['presence_no_rank'] = presence_no_rank
+                    temp_dict['presence_rank'] = None
+                    temp_dict['label'] = label 
+                    temp_list.append(temp_dict.copy())
+
+    mod_df = pd.DataFrame(temp_list)
+    return mod_df
+
+
+def get_df_for_regression_comments(df, all_crawls, labels_df):
+
+    reg_df = pd.DataFrame(columns=['account', 'urlid', 'comment', 'gender', 'ideology', 'location', 'presence_no_rank', 'presence_rank', 'label'])
+
+    temp_list = []
+    for crawl1 in all_crawls:
+        for crawl2 in all_crawls:
+
+            if crawl1 == crawl2:
+                continue
+
+            temp_dict = {}
+
+            mod_df = compute_presence_comments(crawl1, crawl2, df, labels_df)
+            # temp_df = mod_df[['account', 'urlid', 'presence_no_rank', 'presence_rank']].drop_duplicates()
+            temp_df = mod_df.copy()
+
+            gender, ideology, location = compute_label_reg(crawl1, crawl2)
+
+            temp_dict['location'] = location
+            temp_dict['gender'] = gender
+            temp_dict['ideology'] = ideology
+
+            var1 = min(crawl1, crawl2)
+            var2 = max(crawl1, crawl2)
+            temp_dict['setup_a'] = var1
+            temp_dict['setup_b'] = var2
+            temp_dict['pair_id'] = f'{var1}_{var2}'
+
+            for index, row in temp_df.iterrows():
+                temp_dict['urlid'] = row['urlid']
+                temp_dict['account'] = row['account']
+                temp_dict['comment'] = row['comment']
+                temp_dict['presence_no_rank'] = row['presence_no_rank']
+                temp_dict['presence_rank'] = row['presence_rank']
+                temp_dict['label'] = row['label']
+                temp_list.append(temp_dict.copy())
+
+
+    reg_df = pd.DataFrame(temp_list)       
+    return reg_df
